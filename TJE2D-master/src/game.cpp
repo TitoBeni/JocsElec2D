6da7bba18860a4947bf276a8f5a07c55d3daf99b
Joel-Benito <joel.benito01@estudiant.upf.edu>
2023-04-15 11:52:22 +0200

    Current version

---
+++ TJE2D-master/src/game.cpp
@@ -0,0 +1,314 @@
+#include "game.h"
+#include "utils.h"
+#include "input.h"
+#include "image.h"
+
+#include <cmath>
+
+Game* Game::instance = NULL;
+
+Image font;
+Image minifont;
+Image sprite;
+Color bgcolor(130, 80, 100);
+
+int speed = 80;
+const float GRAVITY = 9.81f;
+const float groundLevel = 50;
+
+enum {
+	stand, left, right, up
+};
+
+struct Player {
+	Vector2 position;
+	int orientation = stand;
+	bool moving = false;
+	bool isGround = true;
+}player;
+
+Game::Game(int window_width, int window_height, SDL_Window* window)
+{
+	this->window_width = window_width;
+	this->window_height = window_height;
+	this->window = window;
+	this->renderer = NULL;
+	instance = this;
+	must_exit = false;
+
+	fps = 0;
+	frame = 0;
+	time = 0.0f;
+	elapsed_time = 0.0f;
+	verticalVelocity = 0.0f;
+
+	font.loadTGA("data/bitmap-font-white.tga"); //load bitmap-font image
+	minifont.loadTGA("data/mini-font-white-4x6.tga"); //load bitmap-font image
+	sprite.loadTGA("data/gotica_culona.tga"); //example to load an sprite
+	sprite.scale(56, 72);
+
+	//enableAudio(); //enable this line if you plan to add audio to your application
+	//synth.playSample("data/coin.wav",1,true);
+	//synth.osc1.amplitude = 0.5;
+}
+
+//what to do when the image has to be draw
+void Game::render(void)
+{
+	//Create a new Image (or we could create a global one if we want to keep the previous frame)
+	Image framebuffer(160, 120); //do not change framebuffer size
+
+	//add your code here to fill the framebuffer
+	//...
+
+	//some new useful functions
+		framebuffer.fill( bgcolor );								//fills the image with one color
+		//framebuffer.drawLine( 0, 0, 100,100, Color::RED );		//draws a line
+		//framebuffer.drawImage( sprite, 0, 0 );					//draws full image
+		//framebuffer.drawImage( sprite, 0, 0, framebuffer.width, framebuffer.height );   //draws a scaled image
+		//framebuffer.drawImage( sprite, player.position.x, player.position.y, Area((int (time * 4) % 4) * 14, player.orientation * 18,14,18) );	//draws only a part of an image
+		//framebuffer.drawText( "Hello World", 0, 0, font );				//draws some text using a bitmap font in an image (assuming every char is 7x9)
+		//framebuffer.drawText( toString(time), 1, 10, minifont,4,6);	//draws some text using a bitmap font in an image (assuming every char is 4x6)
+
+		if (player.moving) {
+			framebuffer.drawImage(sprite, player.position.x, 40 + player.position.y, Area((int(time * 4) % 4) * 14, player.orientation * 18, 14, 18));
+		}
+		else {
+			framebuffer.drawImage(sprite, player.position.x, 40 + player.position.y, Area(0, player.orientation * 18, 14, 18));
+		}
+
+	//send image to screen
+	showFramebuffer(&framebuffer);
+}
+
+void Game::update(double seconds_elapsed)
+{
+	player.moving = false;
+	player.isGround = true;
+	player.orientation = stand;
+
+	// Gravity
+	if (player.position.y < groundLevel) {
+		player.position.y += GRAVITY * seconds_elapsed * 10;
+	}
+
+	if (player.isGround) {
+
+		if (Input::isKeyPressed(SDL_SCANCODE_UP)) //if key up
+		{
+			//player.position.y -= verticalVelocity * seconds_elapsed * speed;
+			//verticalVelocity -= GRAVITY * seconds_elapsed;
+
+			player.orientation = stand;
+			player.moving = true;
+			player.isGround = false;
+
+			if (verticalVelocity >= 0.0f && player.position.y > -30) {
+				verticalVelocity = 250.0f;
+				player.position.y -= verticalVelocity * seconds_elapsed;
+			}
+
+		}
+		
+	}
+
+	//Read the keyboard state, to see all the keycodes: https://wiki.libsdl.org/SDL_Keycode
+	
+	if (Input::isKeyPressed(SDL_SCANCODE_DOWN)) //if key down
+	{
+		//player.position.y += seconds_elapsed * speed;
+		//player.orientation = down;
+		//player.moving = true;
+	}
+	if (Input::isKeyPressed(SDL_SCANCODE_RIGHT)) //if key right
+	{
+		player.position.x += seconds_elapsed * speed;
+		player.orientation = right;
+		player.moving = true;
+	}
+	if (Input::isKeyPressed(SDL_SCANCODE_LEFT)) //if key left
+	{
+		player.position.x -= seconds_elapsed * speed;
+		player.orientation = left;
+		player.moving = true;
+	}
+
+	if (Input::wasKeyPressed(SDL_SCANCODE_SPACE)) //if key A was pressed
+	{
+		/*player.orientation = stand;
+		player.moving = true;
+
+		if (verticalVelocity >= 0.0f && player.position.y > 40) {
+			verticalVelocity = 1000.0f;
+			player.position.y -= verticalVelocity * seconds_elapsed;
+		}*/
+		/*else {
+			verticalVelocity = 0.0f;
+		}*/
+	}
+
+	//to read the gamepad state
+	if (Input::gamepads[0].isButtonPressed(A_BUTTON)) //if the A button is pressed
+	{
+	}
+
+	if (Input::gamepads[0].direction & PAD_UP) //left stick pointing up
+	{
+		bgcolor.set(0, 255, 0);
+	}
+
+}
+
+//Keyboard event handler (sync input)
+void Game::onKeyDown( SDL_KeyboardEvent event )
+{
+	switch(event.keysym.sym)
+	{
+		case SDLK_ESCAPE: must_exit = true; break; //ESC key, kill the app
+	}
+}
+
+void Game::onKeyUp(SDL_KeyboardEvent event)
+{
+}
+
+void Game::onGamepadButtonDown(SDL_JoyButtonEvent event)
+{
+
+}
+
+void Game::onGamepadButtonUp(SDL_JoyButtonEvent event)
+{
+
+}
+
+void Game::onMouseMove(SDL_MouseMotionEvent event)
+{
+}
+
+void Game::onMouseButtonDown( SDL_MouseButtonEvent event )
+{
+}
+
+void Game::onMouseButtonUp(SDL_MouseButtonEvent event)
+{
+}
+
+void Game::onMouseWheel(SDL_MouseWheelEvent event)
+{
+}
+
+void Game::onResize(int width, int height)
+{
+    std::cout << "window resized: " << width << "," << height << std::endl;
+	#ifdef USE_OPENGL
+		glViewport( 0,0, width, height );
+	#endif
+	window_width = width;
+	window_height = height;
+}
+
+//sends the image to the framebuffer of the GPU
+void Game::showFramebuffer(Image* img)
+{
+	float startx = -1.0; float starty = -1.0;
+	float width = 2.0; float height = 2.0;
+
+	//center in window
+	float real_aspect = window_width / (float)window_height;
+	float desired_aspect = img->width / (float)img->height;
+	float diff = desired_aspect / real_aspect;
+	width *= diff;
+	startx = -diff;
+
+#ifdef USE_OPENGL
+	static GLuint texture_id = -1;
+	static GLuint shader_id = -1;
+	if (!texture_id)
+		glGenTextures(1, &texture_id);
+
+	//upload as texture
+	glBindTexture(GL_TEXTURE_2D, texture_id);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, img->width, img->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img->pixels);
+	glDisable(GL_CULL_FACE); glDisable(GL_DEPTH_TEST); glEnable(GL_TEXTURE_2D);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0, 0.0); glVertex2f(startx, starty + height);
+	glTexCoord2f(1.0, 0.0); glVertex2f(startx + width, starty + height);
+	glTexCoord2f(1.0, 1.0); glVertex2f(startx + width, starty);
+	glTexCoord2f(0.0, 1.0); glVertex2f(startx, starty);
+	glEnd();
+#else
+	static SDL_Texture* texture = NULL;
+	static int tex_width = 0;
+	static int tex_height = 0;
+	if (!texture || tex_width != img->width || tex_height != img->height)
+	{
+		if(texture)
+			SDL_DestroyTexture(texture);
+		texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_BGR888, SDL_TEXTUREACCESS_TARGET, img->width, img->height);
+		tex_width = img->width;
+		tex_height = img->height;
+	}
+
+	//SDL_RenderClear(renderer);
+	SDL_Rect rect = { 0,0,img->width,img->height };
+	SDL_UpdateTexture(texture, &rect, img->pixels, img->width*4);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+#endif
+	/* this version resizes the image which is slower
+	Image resized = *img;
+	//resized.quantize(1); //change this line to have a more retro look
+	resized.scale(window_width, window_height);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	if (1) //flip
+	{
+	glRasterPos2f(-1, 1);
+	glPixelZoom(1, -1);
+	}
+	glDrawPixels( resized.width, resized.height, GL_RGBA, GL_UNSIGNED_BYTE, resized.pixels );
+	*/
+}
+
+//AUDIO STUFF ********************
+
+SDL_AudioSpec audio_spec;
+
+void AudioCallback(void*  userdata,
+	Uint8* stream,
+	int    len)
+{
+	static double audio_time = 0;
+
+	memset(stream, 0, len);//clear
+	if (!Game::instance)
+		return;
+
+	Game::instance->onAudio((float*)stream, len / sizeof(float), audio_time, audio_spec);
+	audio_time += len / (double)audio_spec.freq;
+}
+
+void Game::enableAudio()
+{
+	SDL_memset(&audio_spec, 0, sizeof(audio_spec)); /* or SDL_zero(want) */
+	audio_spec.freq = 48000;
+	audio_spec.format = AUDIO_F32;
+	audio_spec.channels = 1;
+	audio_spec.samples = 1024;
+	audio_spec.callback = AudioCallback; /* you wrote this function elsewhere. */
+	if (SDL_OpenAudio(&audio_spec, &audio_spec) < 0) {
+		fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+		exit(-1);
+	}
+	SDL_PauseAudio(0);
+}
+
+void Game::onAudio(float *buffer, unsigned int len, double time, SDL_AudioSpec& audio_spec)
+{
+	//fill the audio buffer using our custom retro synth
+	synth.generateAudio(buffer, len, audio_spec);
+}
